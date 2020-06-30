fabc233faea889075021b5272f843e757c0e1b6e
TalhaAhmed-iai <Talha.ahmed@incline-ai.com>
2020-06-30 12:44:00 +0500

    Updates

---
+++ precache-manifest.2645ccabe51dea311712e639f121d21b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3101415cced5ed5f78986ea8b932132d",
+    "url": "/LabelTool/index.html"
+  },
+  {
+    "revision": "886f0a9902295911814e",
+    "url": "/LabelTool/static/css/main.8d8c823f.chunk.css"
+  },
+  {
+    "revision": "7ff782d4b5fdcd77284d",
+    "url": "/LabelTool/static/js/2.f7afa66a.chunk.js"
+  },
+  {
+    "revision": "fe4387774cf6d14572e413aec2cb9b9a",
+    "url": "/LabelTool/static/js/2.f7afa66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886f0a9902295911814e",
+    "url": "/LabelTool/static/js/main.7d24d742.chunk.js"
+  },
+  {
+    "revision": "7c00ae9dfc4a214073ba",
+    "url": "/LabelTool/static/js/runtime-main.253f3f0a.js"
+  },
+  {
+    "revision": "09facdd36196265f0febd22a7563e762",
+    "url": "/LabelTool/static/media/image0.09facdd3.jpg"
+  },
+  {
+    "revision": "e325bde4f13c83f18be11b9a3d11a6f6",
+    "url": "/LabelTool/static/media/image1.e325bde4.jpg"
+  },
+  {
+    "revision": "01e8001b29eae572c2f1bd9b4632ae68",
+    "url": "/LabelTool/static/media/image2.01e8001b.jpg"
+  },
+  {
+    "revision": "39194024767d591815896f2ef40534e0",
+    "url": "/LabelTool/static/media/image3.39194024.jpeg"
+  },
+  {
+    "revision": "746aa7672e7dd5a52cb898ec138baf59",
+    "url": "/LabelTool/static/media/tool.746aa767.png"
+  }
+]);
